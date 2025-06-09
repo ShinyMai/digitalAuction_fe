79c64992c29b89611d2edc38fc5360bfbd944f90
Quanh4122 <vuanhquang0401@gmail.com>
2025-06-09 22:29:14 +0700

    commit

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -2,6 +2,7 @@ const ROUTERS = {
   // Guest
   HOME: "",
   TIN_TUC: "tin-tuc",
+  POST_AUCTION: 'post-auction',
 };
 
 export default ROUTERS;
--- src/routers/roleBased.routes.tsx
+++ src/routers/roleBased.routes.tsx
@@ -10,7 +10,12 @@ const News = React.lazy(
   () => import("../pages/Anonymous/News/News")
 );
 
+const PostAuction = React.lazy(
+  () => import("../pages/Anonymous/PostAuction/index")
+)
+
 export const guestRoutes = [
   { path: ROUTERS.HOME, element: wrapWithLazy(HomePage) },
   { path: ROUTERS.TIN_TUC, element: wrapWithLazy(News) },
+  { path: ROUTERS.POST_AUCTION, element: wrapWithLazy(PostAuction) }
 ];
