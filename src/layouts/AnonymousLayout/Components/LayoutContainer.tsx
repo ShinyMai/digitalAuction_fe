7d19e0d75b6eeb9dcd6f51a64bcf8aff124c24d1
Shiny <anhyeuemvlxnxx@gmail.com>
2025-06-10 21:31:15 +0700

    vnpt ekyc

--- src/layouts/AnonymousLayout/Components/LayoutContainer.tsx
+++ src/layouts/AnonymousLayout/Components/LayoutContainer.tsx
@@ -1,23 +1,22 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 
 interface props {
-    children?: string | React.JSX.Element | React.JSX.Element[];
+  children?:
+    | string
+    | React.JSX.Element
+    | React.JSX.Element[];
 }
 
 const LayoutContainer = ({ children }: props) => {
+  return (
+    <>
+      <Header />
+      <div>{children}</div>
+      <Footer />
+    </>
+  );
+};
 
-    return (
-        <>
-            <Header />
-            <div>
-                {children}
-            </div>
-            <Footer />
-        </>
-    )
-}
-
-export default LayoutContainer
+export default LayoutContainer;
--- src/routers/app.routes.tsx
+++ src/routers/app.routes.tsx
@@ -1,13 +1,18 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import { guestRoutes } from "./roleBased.routes";
+import {
+  companyRoutes,
+  guestRoutes,
+} from "./roleBased.routes";
 import AuthLoader from "../store/authReduxs/authLoader";
 import { ToastContainer } from "react-toastify";
+import PrivateRoutes from "../layouts/AnonymousLayout";
+import PrivateRoutesCompany from "../layouts/CompanyLayout";
 const AppRouter = () => {
   return (
     <>
       <AuthLoader />
       <Routes>
-        <Route path="/*">
+        <Route path="/app" element={<PrivateRoutes />}>
           {guestRoutes.map((route, index) => (
             <Route
               key={index}
@@ -19,6 +24,18 @@ const AppRouter = () => {
             path="*"
             element={<Navigate to="/not-found" />}
           />
+        </Route>
+        <Route
+          path="/admin"
+          element={<PrivateRoutesCompany />}
+        >
+          {companyRoutes.map((route, index) => (
+            <Route
+              key={index}
+              path={route.path}
+              element={<route.element />}
+            />
+          ))}
         </Route>
       </Routes>
 
